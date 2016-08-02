16efac2a957d7c95ba80a6bab648f0fcc51b77cd
Brandon Richey <brichey@teladoc.com>
2016-08-02 10:23:46 -0400

    Adds markdown support for posts

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Pxblog.Mixfile do
 
   def project do
     [app: :pxblog,
-     version: "0.55555.0",
+     version: "0.6.0",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
@@ -39,7 +39,8 @@ defmodule Pxblog.Mixfile do
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
      {:comeonin, "~> 2.5.2"},
-     {:ex_machina, "~> 1.0"}]
+     {:ex_machina, "~> 1.0"},
+     {:earmark, "~> 1.0.1"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
