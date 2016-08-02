8a80eb2745dd737f69a423c5890232af46995c39
Brandon Richey <brichey@teladoc.com>
2016-08-02 15:11:00 -0400

    Finishes comment support

--- web/controllers/post_controller.ex
+++ web/controllers/post_controller.ex
@@ -5,6 +5,7 @@ defmodule Pxblog.PostController do
 
   plug :assign_user
   plug :authorize_user when action in [:new, :create, :update, :edit, :delete]
+  plug :set_authorization_flag
 
   def index(conn, _params) do
     posts = Repo.all(assoc(conn.assigns[:user], :posts))
@@ -94,9 +95,13 @@ defmodule Pxblog.PostController do
     |> halt
   end
 
-  defp authorize_user(conn, _) do
+  defp is_authorized_user?(conn) do
     user = get_session(conn, :current_user)
-    if user && (Integer.to_string(user.id) == conn.params["user_id"] || Pxblog.RoleChecker.is_admin?(user)) do
+    (user && (Integer.to_string(user.id) == conn.params["user_id"] || Pxblog.RoleChecker.is_admin?(user)))
+  end
+
+  defp authorize_user(conn, _) do
+    if is_authorized_user?(conn) do
       conn
     else
       conn
@@ -104,5 +109,9 @@ defmodule Pxblog.PostController do
       |> redirect(to: page_path(conn, :index))
       |> halt()
     end
+  end
+
+  defp set_authorization_flag(conn, _opts) do
+    assign(conn, :author_or_admin, is_authorized_user?(conn))
   end
 end
